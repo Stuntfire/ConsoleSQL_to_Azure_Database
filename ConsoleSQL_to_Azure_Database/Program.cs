a215c0b8471d11f0082877a8c9bd7ee5e2145619
Søren Roed Schack <soer622t@edu.easj.dk>
2017-09-25 20:59:31 +0200

    Simpel Console App der laver et SELECT * FROM Student-tabellen via min HotelDB på Azure.

--- ConsoleSQL_to_Azure_Database/Program.cs
+++ ConsoleSQL_to_Azure_Database/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,28 @@ namespace ConsoleSQL_to_Azure_Database
     {
         static void Main(string[] args)
         {
+            string SelectAllStudent = "SELECT * FROM Student";
+            string conn =
+                "Server=tcp:hotelserver01.database.windows.net,1433;Initial Catalog=HotelDB;Persist Security Info=False;User ID=sailor;Password=ZAQ12wsx;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            using (SqlConnection databasesConnection = new SqlConnection(conn)) 
+            {
+                SqlCommand command = new SqlCommand(SelectAllStudent, databasesConnection);
+
+                databasesConnection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"Id: {reader.GetInt32(0)} + {reader.GetString(1)} + {reader.GetString(2)}"); 
+                    }
+                }
+                Console.Read();
+                reader.Close();
+            }
         }
     }
 }
---
+++ dbo.Student.data.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO [dbo].[Student] ([Id], [Name], [Klasse]) VALUES (1, N'Asger', N'Børnehaven')
+INSERT INTO [dbo].[Student] ([Id], [Name], [Klasse]) VALUES (2, N'Eva', N'8C')
+INSERT INTO [dbo].[Student] ([Id], [Name], [Klasse]) VALUES (3, N'Andrea', N'1Q')
